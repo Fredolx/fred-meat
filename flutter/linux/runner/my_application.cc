0055a93eeb790c5a510a1e0deb58fbc9f1e1b50d
Frederic Lachapelle <frederic.lachapelle7@gmail.com>
2026-07-17 18:39:17 -0400

    change app name

--- flutter/linux/runner/my_application.cc
+++ flutter/linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "fred_meat");
+    gtk_header_bar_set_title(header_bar, "Fred Meat");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "fred_meat");
+    gtk_window_set_title(window, "Fred Meat");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
--- flutter/windows/runner/main.cpp
+++ flutter/windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"fred_meat", origin, size)) {
+  if (!window.Create(L"Fred Meat", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
